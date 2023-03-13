b0ead340cc2de7277f4df00c032b86f2ece3bf5e
Lucas Emanoel <lucas2014.barros@gmail.com>
2023-03-13 13:43:00 -0300

    Fix: change files

--- main.tf
+++ main.tf
@@ -3,15 +3,13 @@ terraform {
   required_version = ">= 1.3.0"
 
   required_providers {
-
     aws = {
+      version = "4.12.1"
       source  = "hashicorp/aws"
-      version = "3.73.0"
     }
   }
 
 }
-
 
 provider "aws" {
   region = "us-east-1"
@@ -27,8 +25,8 @@ provider "aws" {
 data "terraform_remote_state" "vpc" {
   backend = "s3"
   config = {
-    bucket = "lucasemanoel-remote-state"
-    key    = "aws-vpc/terraform.tfstate"
+    bucket = "remote-state-lucasemanoel-2"
+    key    = "aws-pipeline/terraform.tfstate"
     region = "us-east-1"
   }
 }
--- vm-aws.tf
+++ vm-aws.tf
@@ -4,14 +4,14 @@ resource "aws_key_pair" "key" {
 }
 
 resource "aws_instance" "vm" {
-  ami                         = "ami-0d527b8c289b4af7f"
-  instance_type               = "t2.micro"
-  key_name                    = aws_key_pair.key.key_name
-  subnet_id                   = data.terraform_remote_state.vpc.outputs.subnet_id
-  vpc_security_group_ids      = [data.terraform_remote_state.vpc.outputs.security_group_id]
+  ami           = "ami-006dcf34c09e50022" #ami us-east-1
+  instance_type = "t2.micro"
+  key_name      =  aws_key_pair.key.key_name
+  subnet_id     =  data.terraform_remote_state.vpc.outputs.subnet_id
+  vpc_security_group_ids = [lookup(data.terraform_remote_state.vpc.outputs, "security_group_id", null)]
   associate_public_ip_address = true
 
   tags = {
-    "Name" = "vm-terraform"
+    Name = "vm-terraform"
   }
 }
